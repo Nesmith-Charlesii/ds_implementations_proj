e2088e7ad3b6610b7ef7c4e071ee9e137b3ef978
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-26 22:16:59 -0400

    created a list called 'months_in_year' that will hold the months of the year. Accessed the month of march at index 2 and printed to the console. Created a set to hold locations as values. Added 3 new values to the bday_locations set. Created a class called sweepstakes

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # store months of the year.
+    # uses a list to access at index
+    months_in_year = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+                      'november', 'december']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pi_day = months_in_year[2]
+    print(f"The month of pi is in {pi_day}\n")
+
+    # store locations where you have celebrated your birthday
+    # uses a set to add and iterate over values
+    bday_locations = {'virginia, us', 'north carolina, us', 'california, us', 'drc, africa', 'germany, europe'}
+    bday_locations.add('ghana, africa')
+    bday_locations.add('nevada, us')
+    bday_locations.add('georgia, us')
+    # iterate over the set of values
+    for location in bday_locations:
+        print(location)
+
+    # store sweep_stakes contestants.
+    class Sweepstakes:
+        def __init__(self):
+            self.contestants = {}
+            self.key = 1
+
+        def add_contestant(self, first_name, last_name):
+            self.contestants[self.key] = f"{first_name} {last_name}"
+            self.key += 1
+
+        def select_winner(self):
+            rand = random.randrange(6)
+            winner = self.contestants[rand]
+            print(f"\nAnd the winner is... {winner}!")
+
+    sweepstakes = Sweepstakes()
+    sweepstakes.add_contestant('bruce', 'lee')
+    sweepstakes.add_contestant('jackie', 'chan')
+    sweepstakes.add_contestant('jet', 'li')
+    sweepstakes.add_contestant('michael jai', 'white')
+    sweepstakes.add_contestant('chuck', 'norris')
+    print(f"\n{sweepstakes.contestants}")
+    sweepstakes.select_winner()
+
